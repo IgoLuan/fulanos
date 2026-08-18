1a1e262ad64d312f5a8b533e8b308d2969001b71
EA - Luan Igor Oliveira Costa <luan_igor@educadventista.org>
2026-08-18 08:23:54 -0300

    commit

---
+++ exercicio1.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("button")
+const preco = document.getElementById("preco")
+const quantidade = document.getElementById("quantidade")
+const dinheiro = document.getElementById("dinheiro")
+const paragrafoResultado = document.getElementById("resultado")
+
+button.addEventListener("click", calcularTroco)
+
+function calcularTroco(){
+    const dinheiroDado = dinheiro.value
+    const precoValor = preco.value
+    const numeroItens = quantidade.value
+
+    if(dinheiroDado < precoValor * numeroItens){
+        alert("Dinheiro insuficiente! valor restante a pagar: " + precoValor * numeroItens - dinheiroDado)
+    }else {
+        const troco = dinheiroDado - (precoValor * numeroItens)
+
+        paragrafoResultado.textContent = "Troco: " + troco
+    }
+}
